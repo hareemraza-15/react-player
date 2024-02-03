63966ce2b3c8e41405d2b34989b6bb1ba940615f
hareemr <158578394+hareemr@users.noreply.github.com>
2024-02-03 22:27:34 +0530

    sucess or not

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import ReactPlayer from 'react-player'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+      <ReactPlayer
+        url='/trp.mp4'
+        loop='true'
+        autoplay='true'
+        controls='true'
+      />
+
+      <img src='/trp.gif' alt='hi' className='img' />
+
+    </>
   );
 }
 
